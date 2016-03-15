9fcddd8f34c9ab9d1055726333f4310ceb19ab47
smijiavapicurta <cesarepini@gmail.com>
2016-03-15 20:31:57 +0100

    added the UpdateMovies class

--- app/src/main/java/com/example/cesarepini/moviedatabase/MainActivityFragment.java
+++ app/src/main/java/com/example/cesarepini/moviedatabase/MainActivityFragment.java
@@ -5,6 +5,10 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+
+import java.util.ArrayList;
 
 /**
  * A placeholder fragment containing a simple view.
@@ -17,6 +21,19 @@ public class MainActivityFragment extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
+        View root = inflater.inflate(R.layout.fragment_main, container, false);
+
+        ListView listView = (ListView) root.findViewById(R.id.movies_list_view);
+        ArrayAdapter arrayAdapter = new ArrayAdapter(
+                this.getContext(),
+                R.layout.movies_listview_text_element,
+                R.id.movies_listview_text_element,
+                new ArrayList()
+        );
+        listView.setAdapter(
+                arrayAdapter
+        );
+        
         return inflater.inflate(R.layout.fragment_main, container, false);
     }
 }
---
+++ app/src/main/java/com/example/cesarepini/moviedatabase/UpdateMovies.java
@@ -0,0 +1,21 @@
+package com.example.cesarepini.moviedatabase;
+
+import android.os.AsyncTask;
+
+/**
+ * Created by cesarepini on 15/03/16.
+ * Async Task for updating the movies.
+ */
+public class UpdateMovies extends AsyncTask <Void, Void, Void> {
+
+    /**
+     * TAG for the logs.
+     */
+    public String LOG_TAG = UpdateMovies.class.getSimpleName();
+
+    @Override
+    protected Void doInBackground(Void... params) {
+
+        return null;
+    }
+}
